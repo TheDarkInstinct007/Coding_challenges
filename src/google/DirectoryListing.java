b462f9aa0aa014b8a2c2cfe23a37a3e3e8f7e0bd
tusha <tusha@DESKTOP-0LHUPH6>
2016-11-24 04:08:44 -0600

    Bug fix

--- src/google/DirectoryListing.java
+++ src/google/DirectoryListing.java
@@ -4,7 +4,8 @@ public class DirectoryListing {
 
 	public static void main(String[] args) {
 		
-		String directoryList="dir1\n dir11\n  text1.txt\n  dir111\n   image0.jpeg\n   image1.jpeg\n dir12\n  image2.jpeg\ndir2\n image3.jpeg";
+	//	String directoryList="dir1\n dir11\n  text1.txt\n  dir111\n   image0.jpeg\n   image1.jpeg\n dir12\n  image2.jpeg\ndir2\n image3.jpeg";
+		String directoryList="dir1\n dir11\n  text1.txt\n  dir111\n   image0.jpeg\n   image1.jpeg\n   dir1111\n dir12\n  image2.jpeg\ndir2\n image3.jpeg";
 		System.out.println(directoryList);
 		
 		int totLen=0;
@@ -16,61 +17,33 @@ public class DirectoryListing {
 			if(contents[i].replaceFirst("[^ ]+", "").length()==0)
 				path="";
 			
-
-			if(contents[i].replaceFirst("[^ ]+", "").length() > contents[i-1].replaceFirst("[^ ]+", "").length())
-			{
-				if(contents[i].indexOf('.')!=-1)
-				{
-					if(contents[i].endsWith(".png") || contents[i].endsWith(".jpeg") || contents[i].endsWith(".gif"))
-					{
-						totLen = totLen + path.length() + 1 + contents[i].trim().length();
-						continue;
-					}
-				}
-				else
-				{
-					path = path+"/"+contents[i].trim();
-				}
-			}
-			else if(contents[i].replaceFirst("[^ ]+", "").length() == contents[i-1].replaceFirst("[^ ]+", "").length())
+			if(contents[i].replaceFirst("[^ ]+", "").length() == contents[i-1].replaceFirst("[^ ]+", "").length())
 			{
 				if(contents[i-1].indexOf('.')==-1)
-					path = path.substring(0, path.lastIndexOf("/")); 
-				
-				if(contents[i].indexOf('.')!=-1)
-				{
-					if(contents[i].endsWith(".png") || contents[i].endsWith(".jpeg") || contents[i].endsWith(".gif"))
-					{
-						totLen = totLen + path.length() + 1 + contents[i].trim().length();
-						continue;
-					}
-				}
-				else
-				{
-					path = path+"/"+contents[i].trim();
-				}				
+					path = path.substring(0, path.lastIndexOf("/")); 								
 			}
 			else if(contents[i].replaceFirst("[^ ]+", "").length() < contents[i-1].replaceFirst("[^ ]+", "").length())
 			{
-				for(int j=1; j<= contents[i-1].replaceFirst("[^ ]+", "").length()-contents[i].replaceFirst("[^ ]+", "").length(); j++)
+				String oldPath=path;
+				for(int j=1; j<= oldPath.replaceAll("[^/]+", "").length()-contents[i].replaceFirst("[^ ]+", "").length(); j++)
 				{
 					if(path!="" && path.lastIndexOf("/")!=-1)
 						path = path.substring(0, path.lastIndexOf("/"));
-				}
-				
-				if(contents[i].indexOf('.')!=-1)
-				{
-					if(contents[i].endsWith(".png") || contents[i].endsWith(".jpeg") || contents[i].endsWith(".gif"))
-					{
-						totLen = totLen + path.length() + 1 + contents[i].trim().length();
-						continue;
-					}
-				}
-				else
-				{
-					path = path+"/"+contents[i].trim();
-				}	
+				}				
 			}
+			
+			if(contents[i].indexOf('.')!=-1)
+			{
+				if(contents[i].endsWith(".png") || contents[i].endsWith(".jpeg") || contents[i].endsWith(".gif"))
+				{
+					totLen = totLen + path.length() + 1 + contents[i].trim().length();
+					continue;
+				}
+			}
+			else
+			{
+				path = path+"/"+contents[i].trim();
+			}	
 			
 		}
 		
